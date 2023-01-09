7825c2fa6c5840b34090f830e5d8abafe62bdcb1
Alex Okumu <alexokumu006@gmail.com>
2023-01-08 23:29:23 -0800

    Inheritance of classes

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""inherits from Rectangle (9-rectangle.py)"""
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", self.__size)
+        super().__init__(self.__size, self.__size)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,4 @@
+Inheritance is a technique for creating new classes that are modified versions of existing classes. The new class is known as the subclass, while the existing class is known as the superclass. The subclass inherits attributes and behaviors from the superclass and may have its own attributes and behaviors.
+
+In Python, a subclass is defined by using the class keyword, followed by the subclass name and the superclass name in parentheses.
+  A subclass can also override the attributes and behaviors of the superclass by defining new versions of those attributes and behaviors in the subclass. 
